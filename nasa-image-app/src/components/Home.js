6de3844a9b8768c94a417811f193c2099a08cff5
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2020-11-24 20:36:58 -0500

    Add files via upload
    Nasa image app using the NASA api to display a picture of the day along with a description. The picture was taken in space BY NASA, everyday the api pulls a new picture and description.

---
+++ nasa-image-app/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+    return (
+        <div className="home">
+            <Link className="home-link" to="/nasaphoto">Reach for the Stars!</Link>
+        </div>
+    )
+}
