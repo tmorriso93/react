e9a94338f878475dcc89e150044cd2a11bad9742
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2020-11-26 15:28:45 -0500

    Add files via upload
    Wild Life Tracker built with React & Nasa And the Google Maps API. 
    This App shows active wild fires around the world.

--- wildfire-tracker/src/components/Map.js
+++ wildfire-tracker/src/components/Map.js
@@ -3,27 +3,35 @@ import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 import LocationInfoBox from './LocationInfoBox'
 
+
+
+
 const Map = ({ eventData, center, zoom }) => {
     const [locationInfo, setLocationInfo] = useState(null)
 
     const markers = eventData.map(ev => {
         if(ev.categories[0].id === 8) {
             return <LocationMarker 
+                key={ev.id}
                 lat={ev.geometries[0].coordinates[1]} 
                 lng={ev.geometries[0].coordinates[0]} 
                   onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}  
-                />
+                   />
+                  
         }
         return null
-    })
+    }
+    )
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyCNMY7FRIhOlVtGQeKmihzdNdXMatej2p8' }}
+                bootstrapURLKeys={{ key: `${process.env.REACT_APP_API_KEY}` }}
                 defaultCenter={ center }
                 defaultZoom={ zoom }
+               
             >
                 {markers}
+                
             </GoogleMapReact>
             {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
