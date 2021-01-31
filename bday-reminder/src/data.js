28ad6ec144d902fa38494804b97f658a0357a677
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2021-01-30 20:09:03 -0500

    Add files via upload
    simple react app to display data from an array utilizing useState.
    the data displayed are people with an id, age, name, and image

---
+++ bday-reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Cindy Green',
+    age: 20,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Janet Olive',
+    age: 23,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sean Combs',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Larry Little',
+    age: 27,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lilly Mahone',
+    age: 22,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
