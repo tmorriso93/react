5e4d40873cc597ae229673d9bd9b416926fa70ab
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2022-09-13 13:01:05 -0400

    Add files via upload
    crypto market app 
    monitor trending crypto stocks, charts, and information.
    Made with react, tailwind, and the gecko crypto api

---
+++ crypto-market-app/src/pages/CryptoDetail.jsx
@@ -0,0 +1,14 @@
+
+import CoinDetail from '../components/CoinDetail'
+import HistoryChart from '../components/HistoryChart'
+
+const CryptoDetail = () => {
+  return (
+    <div className='wrapper-container mt-10'>
+      <HistoryChart />
+      <CoinDetail />
+    </div>
+  )
+}
+
+export default CryptoDetail
